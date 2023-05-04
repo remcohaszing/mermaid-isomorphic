83cb47e7f4ad06f80cbb4c494bdeea2625887c6b
Remco Haszing <remcohaszing@gmail.com>
2023-05-04 11:37:14 +0200

    Return a render result object
    The returned object includes the following properties:
    
    - `svg`
    - `id`
    - `screenshot`
    - `width`
    - `height`
    - `title`
    - `description`

--- browser.ts
+++ browser.ts
@@ -1,15 +1,60 @@
 import mermaid from 'mermaid'
 
-import { type MermaidRenderer, type createMermaidRenderer as nodeImplementation } from './index.js'
+import {
+  type MermaidRenderer,
+  type createMermaidRenderer as nodeImplementation,
+  type RenderResult
+} from './index.js'
+
+const parser = new DOMParser()
+
+/**
+ * Get an aria value form a referencing attribute.
+ *
+ * @param element The SVG element the get the value from.
+ * @param attribute The attribute whose value to get.
+ * @returns The aria value.
+ */
+function getAriaValue(element: SVGSVGElement, attribute: string): string | undefined {
+  const value = element.getAttribute(attribute)
+  if (!value) {
+    return
+  }
+
+  let result = ''
+  for (const id of value.split(/\s+/)) {
+    const node = element.getElementById(id)
+    if (node) {
+      result += node.textContent
+    }
+  }
+  return result
+}
 
 const renderer: MermaidRenderer = (diagrams, options) =>
-  // @ts-expect-error We always return a string in the browser.
   Promise.allSettled(
-    diagrams.map((diagram, index) =>
-      mermaid
-        .render(`${options?.prefix ?? 'mermaid'}-${index}`, diagram)
-        .then((result) => result.svg)
-    )
+    diagrams.map(async (diagram, index) => {
+      const id = `${options?.prefix ?? 'mermaid'}-${index}`
+
+      const { svg } = await mermaid.render(id, diagram)
+      const root = parser.parseFromString(svg, 'image/svg+xml')
+      const element = root.firstChild as SVGSVGElement
+      const { height, width } = element.viewBox.baseVal
+      const description = getAriaValue(element, 'aria-describedby')
+      const title = getAriaValue(element, 'aria-labelledby')
+
+      const result: RenderResult = { height, id, svg, width }
+
+      if (description) {
+        result.description = description
+      }
+
+      if (title) {
+        result.title = title
+      }
+
+      return result
+    })
   )
 
 export const createMermaidRenderer: typeof nodeImplementation = () => renderer
