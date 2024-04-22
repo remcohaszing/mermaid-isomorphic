f340fb0157c68fca353c1604a6c82cdc52601bdc
Remco Haszing <remcohaszing@gmail.com>
2024-04-22 10:56:01 +0200

    Use snapshot-fixtures for testing

--- src/test.ts
+++ src/test.ts
@@ -1,194 +1,139 @@
 import assert from 'node:assert/strict'
-import { readdir, readFile, writeFile } from 'node:fs/promises'
+import { writeFile } from 'node:fs/promises'
 import { createRequire } from 'node:module'
-import { after, before, describe, test } from 'node:test'
+import { join } from 'node:path'
+import { after, before, test } from 'node:test'
 import { pathToFileURL } from 'node:url'
 
 import { build } from 'esbuild'
 import { createMermaidRenderer, type RenderResult } from 'mermaid-isomorphic'
 import { type Browser, chromium, firefox } from 'playwright-core'
-import prettier from 'prettier'
+import { testFixturesDirectory } from 'snapshot-fixtures'
 
 const require = createRequire(import.meta.url)
 const irishGrover = pathToFileURL(require.resolve('@fontsource/irish-grover'))
-const fixturesPath = new URL('../fixtures/', import.meta.url)
-const fixtureNames = (await readdir(fixturesPath)).sort()
+let browser: Browser
+let content: string
 
-interface FixtureTest {
-  input: string
-  validate: (actual: RenderResult) => Promise<void>
+async function testFixtureResults(
+  results: PromiseSettledResult<RenderResult>[],
+  pngPath?: string
+): Promise<string> {
+  assert.equal(results.length, 1)
+  const [result] = results
+  assert.equal(result.status, 'fulfilled')
+  const { screenshot, svg, ...meta } = result.value
+  if (pngPath) {
+    await writeFile(pngPath, screenshot!)
+  }
+  return `<!--\n${JSON.stringify(meta, undefined, 2)}\n-->\n${svg}`
 }
 
-async function readFixture(name: string, expectedName: string): Promise<FixtureTest> {
-  const fixturePath = new URL(`${name}/`, fixturesPath)
-  const inputPath = new URL('input.mmd', fixturePath)
-  const pngPath = new URL(`${expectedName}.png`, fixturePath)
-  const expectedPath = new URL(`${expectedName}.svg`, fixturePath)
+before(async () => {
+  const output = await build({
+    bundle: true,
+    entryPoints: [require.resolve('./browser.js')],
+    format: 'cjs',
+    write: false
+  })
+  assert.deepEqual(output.errors, [])
+  assert.deepEqual(output.warnings, [])
+  assert.equal(output.outputFiles.length, 1)
+  content = `(() => {
+    let module = {};
 
-  const input = await readFile(inputPath, 'utf8')
-  let expected: Buffer | string | undefined
-  try {
-    expected = await readFile(expectedPath, 'utf8')
-  } catch {
-    await writeFile(expectedPath, '')
-  }
+    ${output.outputFiles[0].text}
 
-  return {
-    input,
-    async validate({ screenshot, svg, ...meta }) {
-      const normalized = await prettier.format(
-        `<!--\n${JSON.stringify(meta, undefined, 2)}\n-->\n${svg}`,
-        { parser: 'html' }
-      )
-      if (process.argv.includes('update') || !expected) {
-        await writeFile(expectedPath, normalized)
+    Object.assign(window, module.exports)
+  })()`
 
-        if (screenshot) {
-          await writeFile(pngPath, screenshot)
-        }
-      }
-      assert.equal(normalized, expected)
-    }
-  }
-}
+  browser = await chromium.launch({ headless: true })
+})
 
-describe('node', () => {
-  for (const name of fixtureNames) {
-    test(name, async () => {
-      const { input, validate } = await readFixture(name, 'expected')
+after(async () => {
+  await browser?.close()
+})
+
+testFixturesDirectory({
+  directory: new URL('../fixtures', import.meta.url),
+  prettier: true,
+  tests: {
+    async 'expected.svg'(file) {
       const renderer = createMermaidRenderer()
+      const results = await renderer([String(file)], { screenshot: true })
 
-      const results = await renderer([input], { screenshot: true })
+      return testFixtureResults(results, join(file.dirname!, 'expected.png'))
+    },
 
-      assert.equal(results.length, 1)
-      const [result] = results
-      assert.equal(result.status, 'fulfilled')
-
-      await validate(result.value)
-    })
-
-    test(`${name} with options`, async () => {
-      const { input, validate } = await readFixture(name, 'with-options')
+    async 'with-options.svg'(file) {
       const renderer = createMermaidRenderer()
-
-      const results = await renderer([input], {
+      const results = await renderer([String(file)], {
         mermaidConfig: { theme: 'dark' },
         prefix: 'prefix'
       })
 
-      assert.equal(results.length, 1)
-      const [result] = results
-      assert.equal(result.status, 'fulfilled')
+      return testFixtureResults(results)
+    },
 
-      await validate(result.value)
-    })
-
-    test(`${name} custom font`, async () => {
-      const { input, validate } = await readFixture(name, 'custom-font')
+    async 'custom-font.svg'(file) {
       const renderer = createMermaidRenderer()
-
-      const results = await renderer([input], {
+      const results = await renderer([String(file)], {
         css: irishGrover,
         mermaidConfig: { fontFamily: '"Irish Grover"' }
       })
 
-      assert.equal(results.length, 1)
-      const [result] = results
-      assert.equal(result.status, 'fulfilled')
+      return testFixtureResults(results)
+    },
 
-      await validate(result.value)
-    })
-
-    test(`${name} firefox`, async () => {
-      const { input, validate } = await readFixture(name, 'firefox')
+    async 'firefox.svg'(file) {
       const renderer = createMermaidRenderer({ browser: firefox })
+      const results = await renderer([String(file)])
 
-      const results = await renderer([input])
+      return testFixtureResults(results)
+    },
 
-      assert.equal(results.length, 1)
-      const [result] = results
-      assert.equal(result.status, 'fulfilled')
+    async 'browser.svg'(file) {
+      const page = await browser.newPage()
+      await page.addScriptTag({ content })
 
-      await validate(result.value)
-    })
-  }
-
-  test('handle errors', async () => {
-    const renderer = createMermaidRenderer({ browser: chromium })
-    const results = await renderer(['invalid'])
-
-    assert.equal(results.length, 1)
-    const [result] = results
-    assert.strictEqual(result.status, 'rejected' as const)
-    assert(result.reason instanceof Error)
-    assert.equal(result.reason.name, 'UnknownDiagramError')
-    assert.match(result.reason.stack!, /\/node_modules\/mermaid\/dist\/mermaid\.js:\d+:\d+/)
-  })
-
-  test('concurrent rendering', async () => {
-    const renderer = createMermaidRenderer({ browser: chromium })
-
-    const results = await Promise.all([
-      renderer(['graph TD;\nA-->B']),
-      renderer(['invalid']),
-      renderer(['graph TD;\nC-->D'])
-    ])
-    assert.strictEqual(results[0][0].status, 'fulfilled')
-    assert.strictEqual(results[1][0].status, 'rejected')
-    assert.strictEqual(results[2][0].status, 'fulfilled')
-  })
-})
-
-describe('browser', () => {
-  let browser: Browser
-  let content: string
-
-  before(async () => {
-    const output = await build({
-      bundle: true,
-      entryPoints: [require.resolve('./browser.js')],
-      format: 'cjs',
-      write: false
-    })
-    assert.deepEqual(output.errors, [])
-    assert.deepEqual(output.warnings, [])
-    assert.equal(output.outputFiles.length, 1)
-    content = `(() => {
-      let module = {};
-
-      ${output.outputFiles[0].text}
-
-      Object.assign(window, module.exports)
-    })()`
-
-    browser = await chromium.launch({ headless: true })
-  })
-
-  after(async () => {
-    await browser?.close()
-  })
-
-  test('generate diagram', async () => {
-    const page = await browser.newPage()
-    await page.addScriptTag({ content })
-
-    const { input, validate } = await readFixture('simple', 'browser')
-    const results = await page.evaluate(
-      (diagram) =>
-        createMermaidRenderer()([
-          `%%{
+      const results = await page.evaluate(
+        (diagram) =>
+          createMermaidRenderer()([
+            `%%{
             init: {
               "fontFamily": "arial,sans-serif"
             }
           }%%\n${diagram}`
-        ]),
-      input
-    )
+          ]),
+        String(file)
+      )
 
-    assert.equal(results.length, 1)
-    const [result] = results
-    assert.equal(result.status, 'fulfilled')
+      return testFixtureResults(results)
+    }
+  }
+})
 
-    await validate(result.value)
-  })
+test('handle errors', async () => {
+  const renderer = createMermaidRenderer({ browser: chromium })
+  const results = await renderer(['invalid'])
+
+  assert.equal(results.length, 1)
+  const [result] = results
+  assert.strictEqual(result.status, 'rejected' as const)
+  assert(result.reason instanceof Error)
+  assert.equal(result.reason.name, 'UnknownDiagramError')
+  assert.match(result.reason.stack!, /\/node_modules\/mermaid\/dist\/mermaid\.js:\d+:\d+/)
+})
+
+test('concurrent rendering', async () => {
+  const renderer = createMermaidRenderer({ browser: chromium })
+
+  const results = await Promise.all([
+    renderer(['graph TD;\nA-->B']),
+    renderer(['invalid']),
+    renderer(['graph TD;\nC-->D'])
+  ])
+  assert.strictEqual(results[0][0].status, 'fulfilled')
+  assert.strictEqual(results[1][0].status, 'rejected')
+  assert.strictEqual(results[2][0].status, 'fulfilled')
 })
