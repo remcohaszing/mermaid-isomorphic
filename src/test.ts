d397053612f7952b1d1cfe7bb57b1c4e61acff67
Constantin <constja@users.noreply.github.com>
2026-01-27 12:36:09 +0100

    Add more distinct test svg

--- src/test.ts
+++ src/test.ts
@@ -101,7 +101,7 @@ testFixturesDirectory({
           prefix: 'custom-icons',
           icons: {
             rect: {
-              body: '<g><rect width="80" height="80" x="0" y="0" fill="blue" /></g>',
+              body: '<path fill-rule="evenodd" clip-rule="evenodd" d="M8 16L3.54223 12.3383C1.93278 11.0162 1 9.04287 1 6.96005C1 3.11612 4.15607 0 8 0C11.8439 0 15 3.11612 15 6.96005C15 9.04287 14.0672 11.0162 12.4578 12.3383L8 16ZM3 6H5C6.10457 6 7 6.89543 7 8V9L3 7.5V6ZM11 6C9.89543 6 9 6.89543 9 8V9L13 7.5V6H11Z" fill="#000000"/>',
               width: 80,
               height: 80
             }
