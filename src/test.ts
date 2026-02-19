dd472f8bb9ce42c978ebe9f36403566b4caf6211
Constantin <constantin-marius.jaeck@volkswagen.de>
2026-02-19 12:00:04 +0100

    Support custom iconpacks (#21)

--- src/test.ts
+++ src/test.ts
@@ -93,6 +93,30 @@ testFixturesDirectory({
       return testFixtureResults(results)
     },
 
+    async 'custom-icon-pack.svg'(file) {
+      const renderer = createMermaidRenderer()
+
+      const results = await renderer([String(file)], {
+        iconPacks: [
+          {
+            name: 'custom-icons',
+            icons: {
+              prefix: 'custom-icons',
+              icons: {
+                alien: {
+                  body: '<path fill-rule="evenodd" clip-rule="evenodd" d="M8 16L3.54223 12.3383C1.93278 11.0162 1 9.04287 1 6.96005C1 3.11612 4.15607 0 8 0C11.8439 0 15 3.11612 15 6.96005C15 9.04287 14.0672 11.0162 12.4578 12.3383L8 16ZM3 6H5C6.10457 6 7 6.89543 7 8V9L3 7.5V6ZM11 6C9.89543 6 9 6.89543 9 8V9L13 7.5V6H11Z" fill="#000000"/>',
+                  width: 80,
+                  height: 80
+                }
+              }
+            }
+          }
+        ]
+      })
+
+      return testFixtureResults(results)
+    },
+
     async 'firefox.svg'(file) {
       const renderer = createMermaidRenderer({ browserType: firefox })
       const results = await renderer([String(file)])
